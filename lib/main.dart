d94240dba64fe92cc4d7f93605bc76d006b98581
mudapa <mudanpa@gmail.com>
2022-12-21 03:27:07 +0700

    Add Onboarding : Page, Component, Image, Title, Indicator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:bookingapp/data/src/img_string.dart';
-import 'package:bookingapp/presentation/widget/primary_button.dart';
-import 'package:bookingapp/presentation/widget/secondary_button.dart';
+import 'package:bookingapp/presentation/pages/pages.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const Main());
@@ -12,30 +10,11 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(ImgString.standing),
-              const SizedBox(height: 12),
-              const Text("Test App"),
-              const SizedBox(height: 12),
-              PrimaryButton(
-                onPressed: () {},
-                text: "Download",
-                type: PrimaryButtonType.type3,
-              ),
-              const SizedBox(height: 6),
-              SecondaryButton(
-                onPressed: () {},
-                text: "Download",
-                type: SecondaryButtonType.type3,
-              ),
-            ],
-          ),
-        ),
+      title: 'Booking App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const OnboardingPage(),
     );
   }
 }
---
+++ lib/presentation/pages/onboarding/component/onboarding_title.dart
@@ -0,0 +1,33 @@
+import 'package:bookingapp/config/config.dart';
+import 'package:bookingapp/presentation/pages/onboarding/onboarding_cubit.dart';
+import 'package:bookingapp/presentation/pages/onboarding/onboarding_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class OnboardingTitle extends StatelessWidget {
+  const OnboardingTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = BlocProvider.of<OnboardingCubit>(context);
+    return BlocBuilder<OnboardingCubit, OnboardingState>(
+      builder: (context, state) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              cubit.state.onboardingList[cubit.state.currentIndex].title!,
+              style: AppFont.display3,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              cubit.state.onboardingList[cubit.state.currentIndex].description!,
+              style: AppFont.paragraphMedium,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
